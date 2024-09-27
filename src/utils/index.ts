b872519d0a2539bd9462cec5647f0dfd23234901
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2024-09-27 12:56:09 +0200

    fix: remove non-existing import

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './calendar-dates';
-export * from './generate-color-code.utils';
 export * from './generate-product-code.utils';
 export * from './generate-random-password';
 export * from './id-formatter';
