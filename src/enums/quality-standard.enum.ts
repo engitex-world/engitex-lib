b77b96e25791b088e92412e876fdc18068ce1ea7
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2024-10-09 10:21:14 +0200

    feat: added quality-standards

---
+++ src/enums/quality-standard.enum.ts
@@ -0,0 +1,23 @@
+export enum QualityStandardType {
+  ISO_3801 = 'ISO_3801',
+  ISO_105_C06 = 'ISO_105-C06',
+  ISO_105_B02 = 'ISO_105-B02',
+  ISO_13934 = 'ISO_13934-1',
+  ISO_13937 = 'ISO_13937-2',
+  ISO_5077 = 'ISO_5077',
+  ISO_12947 = 'ISO_12947-2',
+  ISO_12945 = 'ISO_12945-2',
+  ISO_4920 = 'ISO_4920',
+  ISO_9237 = 'ISO_9237',
+}
+
+export enum QualityStandardUnitType {
+  GRAMS_PER_SQUARE_METER = 'g/m²',
+  PERCENTAGE = 'percentage',
+  GRADING_SCALE_1_TO_5 = '1 to 5 scale',
+  GRADING_SCALE_1_TO_8 = '1 to 8 scale',
+  NEWTON = 'N',
+  CYCLES = 'cycles',
+  SPRAY_RATING = '0 to 100 rating',
+  LITERS_PER_SQUARE_METER_SECOND = 'L/m²/s',
+}
