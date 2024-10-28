d3a66768ae3e957873d2cdb31fc21e15792a9338
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2024-10-28 11:19:36 +0000

    feat: update getColorSortingByHexCode

--- src/utils/color-sorting-by-hex-code.utils.ts
+++ src/utils/color-sorting-by-hex-code.utils.ts
@@ -1,14 +1,18 @@
 import { hexToHsl } from './hex-to-hsl-converter.utils';
 
 // Function to get the color priority based on tone
-export function getColorSortingByHexCode(hex: string): number {
-  const { h } = hexToHsl(hex);
+export function getColorSortingByHexCode(hex?: string): number {
+  if (hex) {
+    const { h, l } = hexToHsl(hex);
 
-  // Yellow (roughly 50-60 degrees), Red (0-30 or 330-360), Blue (200-250)
-  if (h >= 50 && h <= 60) return 1; // Yellow
-  if (h > 30 && h < 50) return 2; // Orange
-  if ((h >= 0 && h <= 30) || (h >= 330 && h <= 360)) return 3; // Red
-  if (h >= 200 && h <= 250) return 4; // Blue
+    // Yellow (roughly 50-60 degrees), Red (0-30 or 330-360), Blue (200-250)
+    if (h >= 50 && h <= 60) return 1; // Yellow
+    if (h > 30 && h < 50) return 2; // Orange
+    if ((h > 0 && h <= 30) || (h === 0 && l !== 0 && h <= 30) || (h >= 330 && h <= 360)) return 3; // Red
+    if (h >= 200 && h <= 250) return 4; // Blue
 
-  return 5; // Others
+    return 5; // Others
+  } else {
+    return 99;
+  }
 }
