b112900effb464170b79e20a6c569095dcd0f2eb
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2024-11-01 12:38:36 +0100

    feat: color-palette shades

--- src/utils/color-palette.utils.ts
+++ src/utils/color-palette.utils.ts
@@ -1,4 +1,4 @@
-import { ColorToneType } from '../enums/color.enum';
+import { ColorIntensityType, ColorToneType } from '../enums/color.enum';
 
 export const COLOR_PRODUCT_PALETTE = [
   '#FFFF00', // Yellow
@@ -10,6 +10,7 @@ export const COLOR_PRODUCT_PALETTE = [
   '#40E0D0', // Turquoise
   '#FFFFFF', // White
 ];
+
 export const COLOR_TONES_PALETTE = [
   { type: ColorToneType.RED, shades: ['#FFCDD2', '#E57373', '#B71C1C'] }, // Light, Medium, Dark Red
   { type: ColorToneType.ORANGE, shades: ['#FFE0B2', '#FFB74D', '#E65100'] }, // Light, Medium, Dark Orange
@@ -20,10 +21,49 @@ export const COLOR_TONES_PALETTE = [
   { type: ColorToneType.PINK, shades: ['#F8BBD0', '#F06292', '#880E4F'] }, // Light, Medium, Dark Pink
   { type: ColorToneType.BROWN, shades: ['#D7CCC8', '#A1887F', '#3E2723'] }, // Light, Medium, Dark Brown
   { type: ColorToneType.GRAY, shades: ['#F5F5F5', '#BDBDBD', '#212121'] }, // Light, Medium, Dark Gray
-  { type: ColorToneType.BLACK, shades: ['#E0E0E0', '#757575', '#000000'] }, // Light, Medium, Dark Black (Black at the darkest)
-  { type: ColorToneType.WHITE, shades: ['#FFFFFF', '#FAFAFA', '#F5F5F5'] }, // Light, Medium, Dark White (Lightest tones possible)
+  { type: ColorToneType.BLACK, shades: ['#000000'] }, // Single shade of Black
+  { type: ColorToneType.WHITE, shades: ['#FFFFFF', '#F3EFE0'] }, // Pure White and Ecru White
 ];
 
 export function getTonesPalette() {
-  return COLOR_TONES_PALETTE.flatMap((color) => color.shades);
+  const darkShades: string[] = [];
+  const mediumShades: string[] = [];
+  const lightShades: string[] = [];
+
+  COLOR_TONES_PALETTE.forEach((color) => {
+    if (color.shades.length === 3) {
+      darkShades.push(color.shades[2]);
+      mediumShades.push(color.shades[1]);
+      lightShades.push(color.shades[0]);
+    } else if (color.type === ColorToneType.BLACK) {
+      darkShades.push(color.shades[0]);
+    } else if (color.type === ColorToneType.WHITE) {
+      lightShades.push(color.shades[0]);
+      mediumShades.push(color.shades[1]);
+    }
+  });
+
+  return [...darkShades, ...mediumShades, ...lightShades];
+}
+
+export function getColorToneAndIntensity(hexCode: string) {
+  // Iterate over each color tone and its shades
+  for (const color of COLOR_TONES_PALETTE) {
+    const shadeIndex = color.shades.indexOf(hexCode.toUpperCase());
+
+    // If hexCode is found, return the color tone and intensity based on index
+    if (shadeIndex !== -1) {
+      const intensity =
+        shadeIndex === 0
+          ? ColorIntensityType.LIGHT
+          : shadeIndex === 1
+          ? ColorIntensityType.MEDIUM
+          : ColorIntensityType.DARK;
+
+      return { tone: color.type, intensity };
+    }
+  }
+
+  // If hex code is not found, return null or an error message
+  return null;
 }
