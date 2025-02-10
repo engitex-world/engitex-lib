b0498bf71684f8bc24503340f8874acda0d937aa
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2025-02-10 14:56:16 +0100

    feat: added OrderDeadlineStatusTranslations

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,1 +1,2 @@
 export * from './country/country.en';
+export * from './order/order.translation';
---
+++ src/translations/order/order.translation.ts
@@ -0,0 +1,46 @@
+import { LocaleType } from '../../enums/locale.enum';
+import { OrderDeadlineStatusType } from '../../enums/order.enum';
+
+type OrderDeadlineStatusTranslationsType = {
+  [key in LocaleType]: {
+    [status in OrderDeadlineStatusType]: string;
+  };
+};
+
+const OrderDeadlineStatusTranslations: OrderDeadlineStatusTranslationsType = {
+  en: {
+    APPROVED: 'Approved',
+    AUTO_APPROVED: 'Auto Approved',
+    REJECTED: 'Rejected',
+    PROPOSED_BY_COMPANY: 'Proposed by Company',
+    PROPOSED_BY_CLIENT: 'Proposed by Client',
+  },
+  es: {
+    APPROVED: 'Aprobado',
+    AUTO_APPROVED: 'Aprobado Automáticamente',
+    REJECTED: 'Rechazado',
+    PROPOSED_BY_COMPANY: 'Propuesto por la Empresa',
+    PROPOSED_BY_CLIENT: 'Propuesto por el Cliente',
+  },
+  pt: {
+    APPROVED: 'Aprovado',
+    AUTO_APPROVED: 'Aprovado Automaticamente',
+    REJECTED: 'Rejeitado',
+    PROPOSED_BY_COMPANY: 'Proposto pela Empresa',
+    PROPOSED_BY_CLIENT: 'Proposto pelo Cliente',
+  },
+  hi: {
+    APPROVED: 'Approved',
+    AUTO_APPROVED: 'Auto Approved',
+    REJECTED: 'Rejected',
+    PROPOSED_BY_COMPANY: 'Proposed by Company',
+    PROPOSED_BY_CLIENT: 'Proposed by Client',
+  },
+};
+
+export const getTranslatedOrderDeadlineStatus = (
+  locale: LocaleType,
+  status: OrderDeadlineStatusType
+) => {
+  return OrderDeadlineStatusTranslations[locale][status] || status;
+};
