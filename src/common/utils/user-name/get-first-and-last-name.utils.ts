bcccfb795a582136c1000a4e49b6917d354ebecd
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2026-01-07 13:24:08 +0100

    added user utils functions

---
+++ src/common/utils/user-name/get-first-and-last-name.utils.ts
@@ -0,0 +1,9 @@
+export function getFirstAndLastName(fullName: string): string {
+  const trimmedName = fullName.trim();
+  const nameParts = trimmedName.split(' ');
+  if (nameParts.length >= 2) {
+    return `${nameParts[0]} ${nameParts[nameParts.length - 1]}`;
+  } else {
+    return nameParts[0];
+  }
+}
