c73b5604aaeb4b3bec7f4bb062c7d4482056f690
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2026-03-30 11:55:23 +0200

    added login request dto

--- src/modules/auth/contracts/login.contract.ts
+++ src/modules/auth/contracts/login.contract.ts
@@ -1,10 +1,5 @@
 import { Utilizador } from '../../utilizadores/types';
 
-export interface LoginRequest {
-  nomeUtilizador: string;
-  password: string;
-}
-
 export interface LoginResponse {
   accessToken: string;
   utilizador: Utilizador;
