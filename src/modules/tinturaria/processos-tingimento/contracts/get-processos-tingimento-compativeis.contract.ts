9f59347f8fd17a99424cf2d1bc8c93fc791f222d
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2026-04-08 12:25:30 +0200

    Update get-processos-tingimento-compativeis.contract.ts

--- src/modules/tinturaria/processos-tingimento/contracts/get-processos-tingimento-compativeis.contract.ts
+++ src/modules/tinturaria/processos-tingimento/contracts/get-processos-tingimento-compativeis.contract.ts
@@ -1,5 +1,11 @@
 import { ArtigoFibra } from '@lib/modules/artigos/enums';
+import { ProcessoTingimento } from '../types';
 
 export type GetProcessosTingimentoCompativeisContract = {
   fibras: ArtigoFibra[];
 };
+
+export type GetProcessosTingimentoCompativeisResponse = {
+  fibras: ArtigoFibra[];
+  processosTingimento: ProcessoTingimento[];
+}[];
