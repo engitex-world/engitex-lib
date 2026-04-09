6167ff489a52f7a63b0404e1ff5d4f53a71396fc
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2026-04-09 13:40:17 +0200

    Update get-amostra-pedido-cor.contract.ts

--- src/modules/laboratorio/pedidos-cor/contracts/get-amostra-pedido-cor.contract.ts
+++ src/modules/laboratorio/pedidos-cor/contracts/get-amostra-pedido-cor.contract.ts
@@ -1,3 +1,34 @@
+import { PedidoCorAmostra } from '../types';
+
+import { ProdutoReceitaLaboratorioUnidade } from '@lib/modules/produtos/enums';
+import { Produto } from '@lib/modules/produtos/types';
+
 export type GetAmostraPedidoCorRequest = {
   amostraId?: string;
+};
+
+export type PedidoCorAmostraResponse = PedidoCorAmostra & {
+  ensaiosTable: AmostraEnsaiosTableDto;
+};
+
+type AmostraEnsaiosTableDto = {
+  ensaios: EnsaioTabelaDto[];
+  processos: ProcessoTabelaDto[];
+};
+
+type EnsaioTabelaDto = {
+  id: number;
+  numeroEnsaio: number;
+};
+
+type ProcessoTabelaDto = {
+  id: number;
+  nome: string;
+  ordem: number;
+  produtos: ProdutoTabelaDto[];
+};
+
+type ProdutoTabelaDto = Produto & {
+  unidade: ProdutoReceitaLaboratorioUnidade;
+  quantidadesPorEnsaio: Record<number, string>; // ensaioId -> quantidade
 };
