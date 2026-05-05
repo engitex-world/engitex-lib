822f9353a985b6e4dd7dd9a524326a888606d15f
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2026-05-05 10:01:35 +0200

    Update index.ts

--- src/modules/laboratorio/pedidos-cor/contracts/index.ts
+++ src/modules/laboratorio/pedidos-cor/contracts/index.ts
@@ -1,5 +1,5 @@
 export * from './clone-amostra.contract';
 export * from './get-amostra-pedido-cor.contract';
-export * from './get-pedido-cor.contract';
+export * from './get-pedidos-cor.contract';
 export * from './update-ensaios-amostra.contract';
 export * from './update-processo-amostra.contract';
