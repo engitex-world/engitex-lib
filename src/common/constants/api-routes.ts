25653b338088d7b4bd1859fdd9c6c96549f892a9
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2026-05-07 14:11:33 +0200

    Update api-routes.ts

--- src/common/constants/api-routes.ts
+++ src/common/constants/api-routes.ts
@@ -104,12 +104,18 @@ export const APIRoutes = {
   [AppModules.PLANEAMENTO]: {
     board: `/${AppModules.PLANEAMENTO}/board`,
     porPlanear: `/${AppModules.PLANEAMENTO}/por-planear`,
+    atribuir: `/${AppModules.PLANEAMENTO}/atribuir`,
+    mover: `/${AppModules.PLANEAMENTO}/mover`,
     faseDetalhe: (id: number) => `/${AppModules.PLANEAMENTO}/fase/${id}`,
+    removerFaseDoPlaneamento: (id: number) => `/${AppModules.PLANEAMENTO}/fase/${id}/planeamento`,
+    updateObservacoes: (id: number) => `/${AppModules.PLANEAMENTO}/fase/${id}/observacoes`,
+    updateDatasPrevistas: (id: number) => `/${AppModules.PLANEAMENTO}/fase/${id}/datas-previstas`,
     execucao: {
       fila: `/${AppModules.PLANEAMENTO}/execucao/fila`,
       iniciar: `/${AppModules.PLANEAMENTO}/execucao/iniciar`,
       terminar: `/${AppModules.PLANEAMENTO}/execucao/terminar`,
-      comentario: (faseId: number) => `/${AppModules.PLANEAMENTO}/execucao/fase/${faseId}/comentario`,
+      comentario: (faseId: number) =>
+        `/${AppModules.PLANEAMENTO}/execucao/fase/${faseId}/comentario`,
     },
   },
 
