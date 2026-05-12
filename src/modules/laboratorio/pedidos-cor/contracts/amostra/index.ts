3d056defdbc5cef10bc211b94142e7c6f6fc8ced
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2026-05-12 16:42:12 +0200

    update import paths

---
+++ src/modules/laboratorio/pedidos-cor/contracts/amostra/index.ts
@@ -0,0 +1,5 @@
+export * from './clone-amostra.contract';
+export * from './get-custo-ensaios-amostra.contract';
+export * from './get-pedido-cor-amostra.contract';
+export * from './update-ensaios-amostra.contract';
+export * from './update-processo-amostra.contract';
