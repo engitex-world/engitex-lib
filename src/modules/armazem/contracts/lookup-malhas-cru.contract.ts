6a96da3f76098ecb561793d803f9644256cfae7b
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2026-07-06 19:14:41 +0300

    Update lookup-malhas-cru.contract.ts

--- src/modules/armazem/contracts/lookup-malhas-cru.contract.ts
+++ src/modules/armazem/contracts/lookup-malhas-cru.contract.ts
@@ -1,11 +1,7 @@
-import { ArtigoEstrutura, ArtigoFibra } from '@lib/modules/artigos/enums';
-
-export type LookupMalhasCruComposicaoFilter = {
-  fibra: ArtigoFibra;
-  percentagem: number;
-};
+import { ArtigoEstrutura } from '@lib/modules/artigos/enums';
+import { ArtigoComposicao } from '@lib/modules/artigos/types';
 
 export type LookupMalhasCruRequest = {
   estrutura?: ArtigoEstrutura;
-  composicao?: LookupMalhasCruComposicaoFilter[];
+  composicao?: ArtigoComposicao[];
 };
