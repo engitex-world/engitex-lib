c67ee8426ea61b799c2c6919a5f645ff74b675d8
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2026-07-31 10:45:25 +0200

    Update get-controlo-qualidade-resultados.contract.ts

--- src/modules/controlo-qualidade/contracts/get-controlo-qualidade-resultados.contract.ts
+++ src/modules/controlo-qualidade/contracts/get-controlo-qualidade-resultados.contract.ts
@@ -1,7 +1,5 @@
 import { PlaneamentoFaseCard } from '@lib/modules/producao/planeamento/contracts';
 
-export type GetControloQualidadeResultadosRequest = Record<string, never>;
-
 export type GetControloQualidadeResultadosResponse = {
   testesEmExecucao: PlaneamentoFaseCard[];
   testesPorIniciar: PlaneamentoFaseCard[];
