864d437072ba720a518bcac41afc0efcaf394da2
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2026-08-11 13:48:39 +0200

    Update create-utilizador.contract.ts

--- src/modules/utilizadores/contracts/create-utilizador.contract.ts
+++ src/modules/utilizadores/contracts/create-utilizador.contract.ts
@@ -1,5 +1,3 @@
-import { ResponseError } from '@lib/common/enums';
-import { ServerActionResult } from '@lib/common/types/server-action.type';
 import { UtilizadorFuncao } from '@lib/modules/utilizadores/enums';
 import { Utilizador } from '@lib/modules/utilizadores/types';
 
@@ -13,4 +11,4 @@ export type CreateUtilizadorRequest = {
   ativo?: boolean;
 };
 
-export type CreateUtilizadorResponse = ServerActionResult<Utilizador, ResponseError>;
+export type CreateUtilizadorResponse = Utilizador & { codigoValidacao: string };
