fcfd8fee5e364d09abcf9762f0a007645cb2f374
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2026-09-09 14:12:04 +0200

    Update index.ts

--- src/modules/controlo-qualidade/types/index.ts
+++ src/modules/controlo-qualidade/types/index.ts
@@ -1,5 +1,5 @@
-export * from './caderno-encargos.type';
 export * from './caderno-encargos-norma.type';
-export * from './controlo-qualidade-resultado.type';
+export * from './caderno-encargos.type';
 export * from './controlo-qualidade-resultado-resumo.type';
+export * from './controlo-qualidade-resultado.type';
 export * from './norma-qualidade.type';
